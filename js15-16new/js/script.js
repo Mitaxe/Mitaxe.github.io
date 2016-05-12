c9be221842b1395c023a70bea186cb7932af05b5
Mitaxe <mitaxe-free@yandex.ru>
2016-05-12 14:03:14 +0300

    some fixes

--- js15-16new/js/script.js
+++ js15-16new/js/script.js
@@ -1,21 +1,24 @@
 $(function () {
 
   $('a').on('click', function () {
-    
+    $('#load').show();
+
     var query = $("input[type='text']").val();
-    $('.riffsy-result').remove();    
+    $('.riffsy-result').remove();
+    $('p').remove();
+    
     $.ajax({
-      url: 'http://api.riffsy.com/v1/search?tag=<'+query+'>&limit=30',
+      url: 'http://api.riffsy.com/v1/search?tag=<' + query + '>&limit=30',
       dataType: "json",
       success: function (data) {
-        
-        if(data.results == 0) {
+
+        $('#load').hide();
+        if (data.results == 0) {
           var msg = '<p>По вашему запросу ничего не найдено :(</p>';
           $('body').append(msg);
           return;
         }
-        else $('p').remove();
-        
+
         var page = $('#riffsy').html();
         var content = tmpl(page, {
           data: data
